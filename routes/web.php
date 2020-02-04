967db6d5d3bf772d483ea953004e446e4893fb31
AdheCode <adhep86@gmail.com>
2020-02-04 17:22:38 +0700

    first commit

---
+++ routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+// Route::get('/admin', 'AdminController@login');
+Route::match(['get', 'post'], '/admin', 'AdminController@login');
+Route::get('/admin/dashboard', 'AdminController@dashboard');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+
+//#6 Make Admin Panel in Laravel 5.6 | Set Admin Dashboard | Header | Footer
