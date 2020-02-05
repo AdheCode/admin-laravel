adb372bd62ed466381d354cf882404a978401e4f
AdheCode <adhep86@gmail.com>
2020-02-05 17:24:07 +0700

    tes

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,10 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Auth;
 
 class AdminController extends Controller
 {
-    public function login () {
+    public function login (Request $request) {
+    	if ($request->isMethod('post')) {
+    		$data = $request->input();
+    		if (Auth::attempt(['email' => $data['email'], 'password' => $data['password'], 'admin' => '1'])) {
+    			return redirect('/admin/dashboard');
+    		} else {
+    			return redirect('/admin')->with('');
+    		}
+    	}
     	return view('admin.admin_login');
     }
 
